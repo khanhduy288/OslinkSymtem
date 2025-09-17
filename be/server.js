c29353ea70f8a4121e898e39c7800ad3166cf734
Khanhlam1186 <khanhyt2002@gmail.com>
2025-09-17 15:47:59 +0700

    mádasD

--- be/server.js
+++ be/server.js
@@ -10,7 +10,7 @@ const jwt = require("jsonwebtoken");
 const app = express();
 const PORT = 5000;
 const SECRET_KEY = "mysecretkey123"; // đổi thành key mạnh hơn trong production
-const WORKER_API = "http://localhost:5001";
+const WORKER_API = "https://06e11082d82b.ngrok-free.app";
 
 app.use(cors());
 app.use(express.json());
